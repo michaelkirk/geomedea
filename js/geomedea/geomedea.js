ff30af45d8ac501939562305f16f544af756f020
michaelkirk <michaelkirk@users.noreply.github.com>
2024-07-16 18:31:59 +0000

    deploy: cb138661e64e0fa43ff3cd025209b1b820494af5

--- js/geomedea/geomedea.js
+++ js/geomedea/geomedea.js
@@ -806,7 +806,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('geomedea_wasm_bg.wasm', import.meta.url);
+        input = new URL('geomedea_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
